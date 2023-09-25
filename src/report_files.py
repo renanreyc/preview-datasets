96ee68ad4c9eefdbda8160cc828e423a91dda967
Renan Rey - Work <renan.reyc@gmail.com>
2023-09-25 17:05:23 -0300

    fixed path error to windows

--- src/report_files.py
+++ src/report_files.py
@@ -2,26 +2,32 @@ import os
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-def handle_files(file_list, save_path, format, resultado):
-    try:
-        for file in file_list:
-            new_name = file.split('/')[-1].split('.')[0]
 
-            resultado.insert("end", "lendo arquivo: " + new_name + "." + format)
+def handle_files(file_list, save_path, format, resultado):
+    for file in file_list:
+        try:
+            separator_by_so = os.path.sep
+            new_name = file.split(separator_by_so)[-1].split(".")[0]
+
+            resultado.insert(
+                "end", "gerando report do arquivo: " + new_name + "." + format + "\n"
+            )
 
             # transforma arquivo em dataframe
-            if format == '.xlsx':
+            if format == ".xlsx":
                 df = pd.read_excel(file)
-            elif format == '.csv':
-                df = pd.read_csv(file)
-            elif format == '.parquet':
-                df = pd.read_parquet(file)   
-            
+            elif format == ".csv":
+                df = pd.read_csv(file, header=0)
+            elif format == ".parquet":
+                df = pd.read_parquet(file)
+
             # faz o profile do df
             profile = ProfileReport(df, title="Profiling Report")
 
             # salva o report em html
-            profile.to_file(save_path + new_name + "_report.html")
-    except Exception as e:
+            profile.to_file(
+                save_path + new_name + "_report.html",
+            )
+        except Exception as e:
             output_error = "error: " + str(e)
-            resultado.insert("end", output_error)
+            resultado.insert("end", output_error + "\n")
